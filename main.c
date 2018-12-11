f89e230cd3220511578ddb92fbdc9275c8198d92
Giovanni Aparecido da Silva Oliveira <giovanniapsoliveira@gmail.com>
2018-12-11 10:54:43 -0200

    saving changes

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <time.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define MAIN_MEMORY_SIZE 64
 #define MAX_VIRTUAL_PAGES 64
@@ -27,15 +28,14 @@ typedef struct {
     int psid;
     int p;
     int fid;
+    int prev;
+    int next;
 } Page;
 
 typedef struct {
     int id;
     int ws[WORKING_SET_LIMIT];
-    int wsc ;
-    int head;
-    int tail;
-    int suspended;
+    int p;
     int pg[MAX_VIRTUAL_PAGES];
     int pgc;
 
@@ -49,34 +49,221 @@ Page pg[MAX_PROCESS_IN_EXECUTION * MAX_PAGES_PER_PROCESS];
 int pgc = 0;
 
 Frame frames[MAIN_MEMORY_SIZE];
-int head = -1, tail = -1;
+int head = -1;
 int frc = 0;
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 
-//int getEmptyFrame(){
-//    int i;
-//    for(i=0;i<MAIN_MEMORY_SIZE;i++){
-//        if(frames[i].page == -1){
-//            return i;
-//        }
-//    }
-//    return -1;
-//}
+
+void insertWs(int psid, int pgid){
+    Process *psaux = &ps[psid];
+    int i;
+    for(i=0;i<WORKING_SET_LIMIT;i++){
+        if(psaux->ws[i]==-1){
+            psaux->ws[i]=pgid;
+            return;
+        }
+    }
+    assert(0);
+}
+
+void removeWs(int psid, int pgid){
+    Process *psaux = &ps[psid];
+    int i;
+    for(i=0;i<WORKING_SET_LIMIT;i++){
+        if(psaux->ws[i]==pgid){
+            psaux->ws[i]=-1;
+        }
+    }
+}
+
+int getWsc(int psid){
+    Process *psaux = &ps[psid];
+    int i=0;
+    while(i<WORKING_SET_LIMIT) {
+        if(psaux->ws[i]!=-1)
+            i++;
+    }
+    return i;
+}
+
+int contains(int *list, int len, int obj){
+    puts("contains");
+    int i;
+    for(i=0;i<len;i++){
+        if(list[i]==obj)
+            return 1;
+    }
+    return 0;
+}
+
+int getLastPage(){
+    puts("getLastPage");
+    if(head==-1)
+        return -1;
+    int pgi = head;
+    while(pg[pgi].next!=-1){
+        pgi = pg[pgi].next;
+    }
+    printf("Page %d is the LRU\n",pgi);
+    return pgi;
+}
+
+int getLRUPage(int psid){
+    puts("getLRUPage");
+    int i = getLastPage();
+    if(psid==-1){
+        return i;
+    }
+    while(!contains(ps[psid].ws,WORKING_SET_LIMIT,i)){
+        i = pg[i].prev;
+    }
+    printf("Page %d is the LRU in process %d working set\n",i,psid);
+    return i;
+}
+
+void bringToFront(int pgid){
+    puts("bringToFront");
+    Page *pgaux = &pg[pgid];
+    Page *pgprev = pgaux->prev!=-1? &pg[pgaux->prev]: NULL;
+    Page *pgnext = pgaux->next!=-1? &pg[pgaux->next]: NULL;
+
+    if(pgprev==NULL) {
+        return;
+    }
+
+    if(pgnext!=NULL){
+        pgprev->next = pgnext->id;
+        pgnext->prev = pgprev->id;
+    }else{
+        pgprev->next = -1;
+    }
+
+    pgaux->next = head;
+    pg[head].prev = pgaux->id;
+}
+
+void printIntArray(int *arr, int len){
+    puts("printIntArray");
+    int i=0;
+    for(i=0;i<len;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+int getEmptyFrame(){
+    puts("getEmptyFrame");
+    int i;
+    for(i=0;i<MAIN_MEMORY_SIZE;i++){
+        if(frames[i].page == -1){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void swapInPage(int pgid){
+    puts("swapInPage");
+    Page *pgaux = &pg[pgid];
+    Process *psaux = &ps[pgaux->psid];
+    int aux = getEmptyFrame();
+    assert(aux!=-1);
+
+    Frame *fraux = &frames[aux];
+
+    fraux->page=pgaux->id;
+    frc++;
+
+    pgaux->p=1;
+    pgaux->fid=fraux->id;
+
+    if(!contains(psaux->ws,WORKING_SET_LIMIT,pgid)){
+        insertWs(psaux->id,pgid);
+    }
+
+    printf("Page %d swapped in\n",pgid);
+}
+
+void swapOutPage(int pgid){
+    puts("swapOutPage");
+    Page *pgaux = &pg[pgid];
+    Frame *fraux = &frames[pgaux->fid];
+    Process *psaux = &ps[pgaux->psid];
+
+    fraux->page=-1;
+    frc--;
+
+    pgaux->p=0;
+    pgaux->fid=-1;
+    printf("Page %d swapped out\n",pgid);
+}
+
+void swapIn(int psid){
+    puts("swapIn");
+    Process *psaux = &ps[psid];
+    int i;
+    for(i=0;i<WORKING_SET_LIMIT;i++){
+        if(psaux->ws[i]!=-1)
+            swapInPage(psaux->ws[i]);
+    }
+    psaux->p=0;
+    printf("Process %d swapped out with working set ",psid);
+    printIntArray(&psaux->ws,WORKING_SET_LIMIT);
+    printf("\n");
+}
+
+void swapOut(int psid){
+    puts("swapOut");
+    Process *psaux = &ps[psid];
+    int i;
+    for(i=0;i<WORKING_SET_LIMIT;i++){
+        swapOutPage(psaux->ws[i]);
+    }
+    psaux->p=1;
+    printf("Process %d swapped out with working set ",psid);
+    printIntArray(psaux->ws,WORKING_SET_LIMIT);
+    printf("\n");
+    puts("swapOutOut");
+
+}
 
 void requestPage(int pgid) {
+    puts("requestPage");
     pthread_mutex_lock(&mutex2);
 
     Page *pgaux = &pg[pgid];
     Process *psaux = &ps[pgaux->psid];
     printf("Process %d requested page %d\n",psaux->id,pgaux->id);
 
-    //update LRU
+    if(!psaux->p){
+        puts("a");
+        while((MAIN_MEMORY_SIZE-frc)<getWsc(psaux->id)){
+            int psid = pg[getLastPage()].psid;
+            swapOut(psid);
+        }
+        puts("b");
+        swapIn(psaux->id);
+    }
+
+    if(getWsc(psaux->id)==WORKING_SET_LIMIT){
+        int lru = getLRUPage(psaux->id);
+        swapOutPage(lru);
+        removeWs(psaux->id,lru);
+    }
+
+    if(frc==MAIN_MEMORY_SIZE){
+        swapOut(pg[getLastPage()].psid);
+    }
+
+    swapInPage(pgid);
+    bringToFront(pgid);
+
     pthread_mutex_unlock(&mutex2);
 }
 
 void *processProcedure(void *arg) {
+    puts("processProcedure");
     while (1) {
         int i = *((int*)arg);
         Process *psaux = &(ps[i]);
@@ -88,12 +275,13 @@ void *processProcedure(void *arg) {
 
 
 void createProcess() {
+    puts("createProcess");
     pthread_t *psthread;
     pthread_mutex_lock(&mutex1);
     Process *psaux = &ps[psc];
     psaux->id = psc;
     psaux->pgc = ((rand() % MAX_PAGES_PER_PROCESS) + 1);
-    printf("%d\n",psaux->pgc);
+
     int i;
     for (i = 0; i < psaux->pgc; i++) {
         Page *pgaux = &pg[pgc];
@@ -101,25 +289,25 @@ void createProcess() {
         pgaux->psid = psaux->id;
         pgaux->p = 0;
         pgaux->fid = -1;
+        pgaux->prev = -1;
+        pgaux->next = -1;
         pgc++;
 
         psaux->pg[i] = pgaux->id;
     }
 
-    psaux->wsc = 0;
-    psaux->head = -1;
-    psaux->tail = -1;
-    psaux->suspended = 1;
+    for(i=0;i<WORKING_SET_LIMIT;i++){
+        psaux->ws[i] = -1;
+    }
+
+    psaux->p = 0;
     psc++;
-    printf("%d\n",psaux->pgc);
+
     psthread = &(psaux->thread);
+    printf("Process %d created with pages from %d to %d\n",psaux->id,psaux->pg[0],psaux->pg[psaux->pgc-1]);
     pthread_mutex_unlock(&mutex1);
 
-    //printf("%d\n",psaux->pgc);
-
     pthread_create(psthread, NULL, processProcedure, &psaux->id);
-
-    printf("Process %d created with pages from %d to %d\n",psaux->id,psaux->pg[0],psaux->pg[psaux->pgc-1]);
 
 }
 
